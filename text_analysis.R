589144874fcb08768b04755abd7a2505ffa51939
Murray David Neuzerling <mdneuzerling@gmail.com>
2018-01-08 16:09:14 +1100

    Fiddling with the Markov chain

--- text_analysis.R
+++ text_analysis.R
@@ -12,10 +12,10 @@ wine_words <- wine %>%
     select(X1, description) %>%
     unnest_tokens(word, description)
 
-#' Using just the first 1000 wine reviews takes a few minutes and roughly half
-#' a gigabyte of RAM. I don't want to even think about training on all 130K
+#' Using just the first 2000 wine reviews takes a few minutes and roughly a 
+#' gigabyte of RAM. I don't want to even think about training on all 130K
 #' reviews.
-fit <- markovchainFit(data = (wine_words %>% filter(X1 <= 1000))$word)
+fit <- markovchainFit(data = (wine_words %>% filter(X1 <= 2000))$word)
 
 gen_review <- function() {
     markovchainSequence(n = 50, markovchain = fit$estimate) %>% 
