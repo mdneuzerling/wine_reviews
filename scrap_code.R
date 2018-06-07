9c21e50b7187efcfd4b80e463db236c5f1cfcd41
Murray David Neuzerling <murray@neuzerling.com>
2018-06-07 22:16:29 +1000

    Clean up, preparation of data for colours

--- scrap_code.R
+++ scrap_code.R
@@ -1,11 +1,30 @@
-
+grape_colour <- function(variety) {
+  wiki <- tryCatch(
+    page_content(
+      language = "en", 
+      project = "wikipedia", 
+      page_name = variety
+    )$parse$text$`*`,
+    error = function(e) {
+      return(NA)
+    }
+  )
+  
+  if (is.na(wiki)) {
+    wiki
+  } else {
+    wiki %>% 
+      substr(regexpr("Color of berry skin", .), nchar(.)) %>% 
+      substr(1, regexpr("</tr>", .)) %>%
+      regmatches(gregexpr("<td>(.*)</td>", .)) %>% 
+      {gsub("<.*?>", "", .)} #remove HTML tags
+  }
+}
 
 
 #' Will take a minute
 vis_dat(wine, warn_large_data = FALSE)
 
-wine %>% ggplot(aes(x = points)) + 
-    geom_histogram(bins = nrow(wine %>% distinct(points)))
 
 wine %>% 
     group_by(variety) %>% 
