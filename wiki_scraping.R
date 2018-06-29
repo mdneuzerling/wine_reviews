b2852c7400673744b0c1ef9a19d911f8336adee7
Murray David Neuzerling <mdneuzerling@gmail.com>
2018-06-29 16:12:00 +1000

    Colour progress! Only 3400 wines remain

--- wiki_scraping.R
+++ wiki_scraping.R
@@ -1,27 +1,34 @@
+library(tidyverse)
 library(rvest)
 
-wiki_page <- read_html("https://en.wikipedia.org/wiki/List_of_grape_varieties")
-wiki_tables <- wiki_page %>% html_nodes("table")
+wiki_tables <- "https://en.wikipedia.org/wiki/List_of_grape_varieties" %>% 
+    read_html %>% 
+    html_nodes("table")
 red_wines <- wiki_tables[[1]] %>% html_table %>% cbind(colour = "red")
 white_wines <- wiki_tables[[2]] %>% html_table %>% cbind(colour = "white")
 rose_wines <- wiki_tables[[3]] %>% html_table %>% cbind(colour = "rosé")
 all_wines <- rbind(red_wines, white_wines, rose_wines)
 
-all_wines %>% 
+varieties <- all_wines %>% 
     rename(
         variety = `Common Name(s)`,
         synonyms = `All Synonyms`,
         country_of_origin = `Country of origin`
     ) %>% 
     select(variety, synonyms, country_of_origin, colour) %>% 
-    unnest(synonyms = strsplit(synonyms, ", "))
-
-colnames(varieties) <- c("variety", "synonyms", "country_of_origin",
-                         "pedigree", "hectared_cultivates", 
-                         "year_of_introduction", "colour")
-
-
-
-swiki_tables %>% {purrr(
-    c(1, 2),
-    function(x) {html_table(wikitables[[x]])})} %>% purrr::reduce(rbind)
+    {rbind(
+        select(., variety, country_of_origin, colour),
+        rename(select(., synonyms, country_of_origin, colour),
+               variety = synonyms)
+    )} %>%
+    unnest(variety = strsplit(variety, ", ")) %>%
+    unnest(variety = strsplit(variety, " / ")) %>% 
+    mutate(
+        variety = gsub("\\.", "", variety), # remove periods 
+        variety = gsub("\\s*\\([^\\)]+\\)", "", variety), # remove brackets and anything within
+        variety = gsub("\\s*\\[[^\\)]+\\]", "", variety) # same for square brackets
+    ) %>% 
+    mutate_all(tolower) %>% 
+    select(variety, colour, country_of_origin) %>% 
+    arrange(variety) %>% 
+    distinct
